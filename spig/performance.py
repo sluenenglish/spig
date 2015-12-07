3e07fc89ea3e900303e66ed201da2c9d69c87890
Sam Luen-English <sluenenglish@gmail.com>
2015-12-07 13:18:40 +0000

    Renamed Performance to TwoLevelModel

--- spig/performance.py
+++ spig/performance.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-class Performance(object):
+class TwoLevelModel(object):
     """Class for holding data and methods to compare performance
 
     Attributes
@@ -9,9 +9,13 @@ class Performance(object):
 
     data : Pandas Dataframe
         Data to analyse and compare performance within
+    first_level, second_level : string
+        Strings containing column names of on which to construct
+        groups in hierarchical models
 
     """
 
-    def __init__(self, data):
+    def __init__(self, data, first_level, second_level):
 
         self.data = data
+
--- spig/tests/test_performance.py
+++ spig/tests/test_performance.py
@@ -6,10 +6,10 @@ from pandas.util.testing import assert_frame_equal
 import pandas as pd
 
 
-class TestPerformance(TestCase):
+class TestTwoLevelModel(TestCase):
     """Test case for a Performance class"""
 
     def test_init(self):
-        data = pd.DataFrame([{'a' : 1}, {'a' : 4}])
-        p = spig.Performance(data)
+        data = pd.DataFrame([{'a' : 1, 'b' : 2}, {'a' : 4, 'b' : 2}])
+        p = spig.TwoLevelModel(data, 'a', 'b')
         assert_frame_equal(p.data, data)
