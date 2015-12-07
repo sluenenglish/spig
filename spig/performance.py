b95540e3929c2f36c85c94cc7b396f241993f4b2
Sam Luen-English <sluenenglish@gmail.com>
2015-12-07 18:52:48 +0000

    Began implementing parameter estimation for hierarchical model
    - Using SEM to predict sigma

--- spig/performance.py
+++ spig/performance.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import pandas as pd
 
 
@@ -9,13 +10,49 @@ class TwoLevelModel(object):
 
     data : Pandas Dataframe
         Data to analyse and compare performance within
+    measure : string
+        String containing column name of performance indicator
     first_level, second_level : string
         Strings containing column names of on which to construct
         groups in hierarchical models
 
     """
 
-    def __init__(self, data, first_level, second_level):
+    def __init__(self, data, measure, grouping):
 
         self.data = data
+        self.measure = measure
+        self.grouping = grouping
+
+
+    def estimate_parameters(self):
+        self.Y_stats = self.data.groupby(self.grouping).agg(['mean', 'count', 'sem'])[self.measure]
+        self.Y_stats.rename(columns={'mean' : 'y_i',
+                                     'count' : 'counts',
+                                     'sem' : 'sigma'}, inplace=True)
+
+        # omit groups with only one member
+        omitted = self.Y_stats.counts > 1
+        self.omitted_groups = self.Y_stats.index[~omitted]
+        self.Y_stats = self.Y_stats[omitted]
+
+        self.Y_var = self.data[self.measure].var()
+        self.mu = self.data[self.measure].mean()
+
+        self.Y_stats['tau2'] = self.Y_stats.apply(lambda s : self.Y_var - s.sigma ** 2, axis = 1)
+
+
+        self.tau2 = self.Y_stats.tau2.mean()
+
+        self.Y_stats['weight_i'] = self.Y_stats.apply(lambda s :
+                                                      self.tau2 / (s.sigma ** 2 + self.tau2),
+                                                      axis = 1)
+
+
+
+
+
+
+
+
 
--- spig/tests/test_performance.py
+++ spig/tests/test_performance.py
@@ -1,4 +1,3 @@
-"""Test for the generic game class"""
 from unittest import TestCase
 import spig
 from pandas.util.testing import assert_frame_equal
@@ -7,9 +6,36 @@ import pandas as pd
 
 
 class TestTwoLevelModel(TestCase):
-    """Test case for a Performance class"""
+    """Test case for a TwoLevelModel class"""
+
+
+    def setUp(self):
+
+        self.data = pd.DataFrame([{'grouping' : 'A', 'measure' : 2},
+                                  {'grouping' : 'B', 'measure' : 3},
+                                  {'grouping' : 'A', 'measure' : 5},
+                                  {'grouping' : 'B', 'measure' : 5},
+                                  {'grouping' : 'C', 'measure' : 2}])
+        self.model = spig.TwoLevelModel(self.data, 'measure', 'grouping')
+
 
     def test_init(self):
-        data = pd.DataFrame([{'a' : 1, 'b' : 2}, {'a' : 4, 'b' : 2}])
-        p = spig.TwoLevelModel(data, 'a', 'b')
-        assert_frame_equal(p.data, data)
+
+        assert_frame_equal(self.model.data, self.data)
+        self.assertEqual(self.model.measure, 'measure')
+        self.assertEqual(self.model.grouping, 'grouping')
+
+
+    def test_estimate_parameters(self):
+
+        self.model.estimate_parameters()
+
+        self.assertEqual(self.model.Y_stats.shape[0] + self.model.omitted_groups.shape[0],
+                         self.data.groupby('grouping').ngroups)
+
+
+
+
+
+
+
