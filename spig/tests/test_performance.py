b900bd549a80ae0e6cf0ffb79cce29092f0cbe1f
Sam Luen-English <sluenenglish@gmail.com>
2015-12-18 13:40:59 +0000

    Changed method of generating dataframes for testing
    - Works now

--- spig/tests/test_performance.py
+++ spig/tests/test_performance.py
@@ -2,18 +2,37 @@ from unittest import TestCase
 import spig
 from pandas.util.testing import assert_frame_equal
 from hypothesis import given, example, assume
-from hypothesis.strategies import text, floats, fixed_dictionaries, composite, lists, sampled_from
+from hypothesis.strategies import text, floats, fixed_dictionaries, composite, lists, sampled_from, integers
 
 import pandas as pd
 
+#@composite
+#def generate_data_and_keys(draw):
+    #measure = draw(text(min_size=1))
+    #grouping = draw(text(min_size=1))
+    ##assume(measure != grouping)
+    #groups = draw(lists(text(), min_size=1, max_size=3))
+    #data = draw(lists(fixed_dictionaries({grouping: sampled_from(groups), measure:floats()}), max_size=20))
+    #return data, measure, grouping
+#
+
 @composite
-def generate_data_and_keys(draw):
-    measure = draw(text(min_size=1))
-    grouping = draw(text(min_size=1))
-    #assume(measure != grouping)
-    groups = draw(lists(text(), min_size=1, max_size=3))
-    data = draw(lists(fixed_dictionaries({grouping: sampled_from(groups), measure:floats()}), max_size=20))
-    return data, measure, grouping
+def generate_data_and_keys(draw, max_length=100, max_groups=20):
+    measure_name = draw(text(min_size=1))
+    groups_name = draw(text(min_size=1))
+
+    group_set = draw(lists(text(), min_size=1, max_size=max_groups))
+
+    size = draw(integers(min_value=1, max_value = max_length))
+    measures = draw(lists(floats(), min_size=size, max_size=size))
+    groups = draw(lists(sampled_from(group_set),
+                        min_size=size, max_size=size))
+    data = [{measure_name: m, groups_name : g}
+                for m,g in zip(measures, groups)]
+
+    data_df = pd.DataFrame(data)
+
+    return measure_name, groups_name, data_df
 
 
 class TestTwoLevelModel(TestCase):
@@ -31,16 +50,19 @@ class TestTwoLevelModel(TestCase):
 
 
     @given(data_and_keys=generate_data_and_keys())
+    @example(data_and_keys=('measure', 'grouping', None))
     def test_init(self, data_and_keys):
-        data, measure, grouping = data_and_keys
 
-        data_df = pd.DataFrame(data)
+        measure_name, groups_name, data_df = data_and_keys
 
-        model = spig.TwoLevelModel(data_df, measure, grouping)
+        if data_df is None:
+            data_df = self.data
+
+        model = spig.TwoLevelModel(data_df, measure_name, groups_name)
 
         assert_frame_equal(model.data, data_df)
-        self.assertEqual(model.measure, measure)
-        self.assertEqual(model.grouping, grouping)
+        self.assertEqual(model.measure, measure_name)
+        self.assertEqual(model.grouping, groups_name)
 
 
 
